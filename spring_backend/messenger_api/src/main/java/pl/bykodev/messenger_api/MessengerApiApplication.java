59c0174c7a1976c666d493814c99b3acdf66dc47
byko <byko.dev@gmail.com>
2023-01-09 00:36:23 +0100

    spring api initialize

---
+++ spring_backend/messenger_api/src/main/java/pl/bykodev/messenger_api/MessengerApiApplication.java
@@ -0,0 +1,13 @@
+package pl.bykodev.messenger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerApiApplication.class, args);
+    }
+
+}
---
+++ spring_backend/messenger_api/src/test/java/pl/bykodev/messenger_api/MessengerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bykodev.messenger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
