7e17480d2b99395810bae98b2770e579475e6667
bykop <byko.dev@gmail.com>
2023-02-27 17:28:40 +0100

    services and security

--- spring_backend/messenger_api/src/main/java/pl/bykodev/messenger_api/database/repository/UserEntityRepository.java
+++ spring_backend/messenger_api/src/main/java/pl/bykodev/messenger_api/database/repository/UserEntityRepository.java
@@ -3,6 +3,9 @@ package pl.bykodev.messenger_api.database;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserEntityRepository extends JpaRepository<UserEntity, String> {
+    Optional<UserEntity> findByUsername(String username);
 }
